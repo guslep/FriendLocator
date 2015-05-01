bcbdcaefdc049a39c01edf32412c1235d169a433
goldylock <guslep@gmail.com>
2015-05-01 11:05:42 -0400

    heroku last try
    last chance

---
+++ Find Me/index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Gus on 10/6/2014.
+ */
+
+//to be removed
+//var httpServer=require("./node server")
+//var router= require("./router")
+//httpServer.start(router.route);
+
+var server = require("./node server");
+var router = require("./router");
+var requestHandler=require("./requestHandler")
+
+var handle = {}
+handle["/"] = requestHandler.start;
+handle["/start"] = requestHandler.start;
+handle["/upload"] = requestHandler.upload;
+handle["/show"]=requestHandler.show;
+
+server.start(router.route,handle);
+
